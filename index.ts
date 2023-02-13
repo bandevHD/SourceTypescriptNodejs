49c8902ef25c248bb9f7279452badfd4d9bfd24b
bantrandevHD <ban.trannhat@hdwebsoft.dev>
2023-02-13 15:46:09 +0700

    Add helmet morgan

--- index.ts
+++ index.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import morgan from 'morgan';
 
 dotenv.config();
 
 const app = express();
+
+app.use(morgan('common'));
 
 const PORT = process.env.PORT || 5000;
 
